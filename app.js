6017fbb1a85ae8d43eea96e5222830674b89675e
Luke Michalakis <michalakisluke@gmail.com>
2021-09-28 15:43:11 -0400

    <Build HTML framework>

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello Node!');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio Complete! Check out index.html to see the output!')
+});
